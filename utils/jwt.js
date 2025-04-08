e1b8f07ad47d924d8d37dc1fea4656e09ce5aa14
WuShF <1481542813@qq.com>
2025-04-08 21:12:35 +0800

    修复了base64的兼容性问题

--- utils/jwt.js
+++ utils/jwt.js
@@ -1,5 +1,25 @@
+export const atobPolyfill = (base64) => {
+  var chars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/';
+  var output = '';
+  var buffer = 0;
+  var bitCount = 0;
+  for (var i = 0; i < base64.length; i++) {
+    var char = base64.charAt(i);
+    if (char === '=') break;
+
+    buffer = (buffer << 6) | chars.indexOf(char);
+    bitCount += 6;
+
+    if (bitCount >= 8) {
+      bitCount -= 8;
+      output += String.fromCharCode((buffer >> bitCount) & 0xFF);
+      buffer &= (1 << bitCount) - 1;
+    }
+  }
+  return output;
+}
 export const parseJwtPayload = (token) => {
   const payload = token.split('.')[1];
-  const decoded = atob(payload);
+  const decoded = atobPolyfill(payload);
   return JSON.parse(decoded);
 }
